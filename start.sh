6659090f8631794fc9757d1f1bb391324d6cc14b
Andrii Loshkovskyi <mail@loshkovskyi.net>
2015-05-11 13:51:47 +0300

    Fix issue 'exec: logstash: not found'

--- start.sh
+++ start.sh
@@ -6,10 +6,11 @@ LOGSTASH_ENTRYPOINT_CMD='logstash'
 
 if [ "$1" == "$LOGSTASH_ENTRYPOINT_CMD" ]; then
     if [ -e "$LOGSTASH_CONF" ]; then
-        "$LOGSTASH_HOME"/bin/logstash -f $LOGSTASH_CONF
+        $LOGSTASH_HOME/bin/logstash -f $LOGSTASH_CONF
         if [ $? != 0 ]; then
             exit 1
         fi
+        set --
     else
         exit 1
     fi
