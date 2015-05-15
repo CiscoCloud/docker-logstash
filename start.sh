bf12b01bbe6bdb8b3a18a41b6197f5d1c13ca8dd
Keith Chambers <chambers_keith@yahoo.com>
2015-05-14 21:33:57 -0700

    Simplify shell script

--- start.sh
+++ start.sh
@@ -1,19 +1,12 @@
 #!/bin/bash -x
 
-LOGSTASH_HOME='/opt/logstash'
+LOGSTASH_HOME="/opt/logstash"
 LOGSTASH_CONF="$LOGSTASH_HOME/conf.d/logstash.conf"
-LOGSTASH_ENTRYPOINT_CMD='logstash'
 
-if [ "$1" == "$LOGSTASH_ENTRYPOINT_CMD" ]; then
-    if [ -e "$LOGSTASH_CONF" ]; then
-        $LOGSTASH_HOME/bin/logstash -f $LOGSTASH_CONF
-        if [ $? != 0 ]; then
-            exit 1
-        fi
-        set --
-    else
-        exit 1
-    fi
+if [[ "$1" == "logstash" && -e "$LOGSTASH_CONF" ]]; then
+    "$LOGSTASH_HOME/bin/logstash" -f "$LOGSTASH_CONF" || exit 1
+else
+    exit 1
 fi
 
 exec "$@"
