bd02373920282fd462dd39c0183ae4c75dad98c1
Andrii Loshkovskyi <mail@loshkovskyi.net>
2015-05-18 11:13:54 +0300

    Fix issues with positional arguments

--- start.sh
+++ start.sh
@@ -5,8 +5,7 @@ LOGSTASH_CONF="$LOGSTASH_HOME/conf.d/logstash.conf"
 
 if [[ "$1" == "logstash" && -e "$LOGSTASH_CONF" ]]; then
     "$LOGSTASH_HOME/bin/logstash" -f "$LOGSTASH_CONF" || exit 1
-else
-    exit 1
+    set --
 fi
 
 exec "$@"
